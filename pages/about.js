3331615a5ef1b0dcaf8c6095d4801fee96def7e3
Powell <Kade.Powell@cox.com>
2021-08-31 08:46:49 -0400

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+      <p>
+        This is an app to find the latest peer led code workshops and demo
+        events
+      </p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
